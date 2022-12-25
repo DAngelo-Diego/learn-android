ec509688c3b77e0887699696e41b5ba3b4b009f8
DAngelo-Diego <dangelodiego967@gmail.com>
2022-12-25 19:20:40 -0300

    All stuff from SplashScreen and OnBoarding Screens

--- app/src/main/java/com/example/learnandroid/navigation/NavGraph.kt
+++ app/src/main/java/com/example/learnandroid/navigation/NavGraph.kt
@@ -6,8 +6,13 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.learnandroid.presentation.screen.home.HomeScreen
+import com.example.learnandroid.presentation.screen.splash.SplashScreen
+import com.example.learnandroid.presentation.screen.welcome.WelcomeScreen
 import com.example.learnandroid.util.Constants.INFO_ID_DETAILS_ARGUMENT_KEY
+import com.google.accompanist.pager.ExperimentalPagerApi
 
+@ExperimentalPagerApi
 @Composable
 fun SetupNavGraph(navHostController: NavHostController) {
     NavHost(
@@ -15,13 +20,13 @@ fun SetupNavGraph(navHostController: NavHostController) {
         startDestination = Screen.Splash.route
     ) {
         composable(route = Screen.Splash.route){
-
+            SplashScreen(navHostController = navHostController)
         }
         composable(route = Screen.Welcome.route){
-
+            WelcomeScreen(navHostController = navHostController)
         }
         composable(route = Screen.Home.route){
-
+            HomeScreen(navHostController = navHostController)
         }
         composable(route = Screen.Details.route,
         arguments = listOf(navArgument(INFO_ID_DETAILS_ARGUMENT_KEY){
--- app/src/main/java/com/example/learnandroid/util/Constants.kt
+++ app/src/main/java/com/example/learnandroid/util/Constants.kt
@@ -6,4 +6,15 @@ object Constants {
 
     const val LANGUAGE_DATA_BASE_TABLE = "language_table"
 
+    const val LANGUAGE_REMOTE_DATABASE_KEY_TABLE = "language_remote_key_table"
+
+    const val LANGUAGES_DATABASE_DATABASE_MODULE = "languages_database_module"
+
+    const val ON_BOARDING_PAGE_COUNT = 3
+
+    const val LAST_VISIBLE_PAGE_COUNT = 2
+
+    const val  PREFERENCES_NAME_DATASTORE_IMPL = "learn_android"
+
+    const val PREFERENCES_KEY = "on_boarding_completed"
 }
